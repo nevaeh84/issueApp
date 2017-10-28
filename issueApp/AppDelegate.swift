84102dd49cd00a65c6469486da776b9134396c73
Kang JeongWoo <nevaeh840@gmail.com>
2017-10-28 17:56:01 +0900

    add git ignore file

--- issueApp/AppDelegate.swift
+++ issueApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import OAuthSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -42,5 +43,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+    func application(_ app: UIApplication, open url: URL, options:[UIApplicationOpenURLOptionsKey: Any] = [:]) -> Bool{
+        if url.host == "oauth-callback" {
+            OAuthSwift.handle(url: url)
+        }
+        return true
+    }
 }
 
---
+++ issueApp/view/LoginViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LoginViewController.swift
+//  issueApp
+//
+//  Created by STA1_KANGJW on 2017. 10. 28..
+//  Copyright © 2017년 STA1_KANGJW. All rights reserved.
+//
+
+import UIKit
+import OAuthSwift
+
+class LoginViewController: UIViewController {
+    
+    let oauth:OAuth2Swift = OAuth2Swift(consumerKey: "989bfcae35ac94eae42c",
+                                        consumerSecret: "6314de9cff2477f65a7991af35d305715a4c4fa0",
+                                        authorizeUrl: "https://github.com/login/oauth/authorize",
+                                        accessTokenUrl: "https://github.com/login/oauth/access_token",
+                                        responseType: "code")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginPress(){
+        oauth.authorize(withCallbackURL: "issueApp://oauth-callback/github", scope: "user,repo", state: "state", success: {(credenial, _, _) in
+            let token = credenial.oauthToken
+            print(token)
+        }, failure: { error in
+            print(error.localizedDescription)
+        })
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
